d0b4b79812193dbe04b3536beaa55438758e776f
rysiek98 <52510889+rysiek98@users.noreply.github.com>
2020-04-01 17:51:48 +0200

    v0.69

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import java.awt.EventQueue;
 
 
-public class Menu {
+public class App {
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             @Override
